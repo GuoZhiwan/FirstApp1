0ef5007438aa192ad7f50042a2d717a1747b14b7
GuoZhiwan <528537391@qq.com>
2020-05-02 20:39:08 +0800

    随意取出币种和汇率来转换

--- app/src/main/java/com/swufe/firstapp/MainActivity.java
+++ app/src/main/java/com/swufe/firstapp/MainActivity.java
@@ -1,17 +1,13 @@
 package com.swufe.firstapp;
 
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity  {
 
---
+++ app/src/main/java/com/swufe/firstapp/Mylist2Activity.java
@@ -0,0 +1,132 @@
+package com.swufe.firstapp;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Mylist2Activity extends ListActivity implements Runnable, AdapterView.OnItemClickListener {
+    private String TAG="mylist2";
+    Handler handler;
+    private ArrayList<HashMap<String,String>> listItems;//存放文字、图片等信息
+    private SimpleAdapter listItemAdapter;//适配器
+    public void run(){
+        List<HashMap<String,String>> retlist=new ArrayList<HashMap<String,String>>();
+
+        //获取网络数据，放入list带回到主线程中
+        Document doc= null;
+        try {
+            Thread.sleep(1000);
+            doc = Jsoup.connect("https://www.boc.cn/sourcedb/whpj/").get();
+            Log.i(TAG, "run: "+ doc.title());
+            Elements tables =doc.getElementsByTag("table");
+            Element table2=tables.get(1);
+            //获取td中的数据
+            Elements tds=table2.getElementsByTag("td");
+            for(int i=0;i<tds.size();i+=8){
+                Element td1=tds.get(i);
+                Element td2=tds.get(i+5);
+                String str1=td1.text();
+                String val=td2.text();
+                Log.i(TAG, "run: "+td1.text()+"==>"+td2.text());
+                HashMap<String,String>map=new HashMap<String,String>();
+                map.put("ItemTitle",str1);
+                map.put("ItemDetail",val);
+                retlist.add(map);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Message msg = handler.obtainMessage(5);
+        msg.obj=retlist;
+        handler.sendMessage(msg);
+    }
+    private void initListView(){
+        listItems=new ArrayList<HashMap<String, String>>();
+        for(int i=0;i<10;i++){
+            HashMap<String,String> map=new HashMap<String,String>();
+            map.put("ItemTitle","Rate= "+i);
+            map.put("ItemDetail","detail"+i);
+            listItems.add(map);
+        }
+        //生成适配器的Item和动态数组对应的元素
+        listItemAdapter=new SimpleAdapter(this,listItems,
+                R.layout.list_item,
+                new String[]{"ItemTitle","ItemDetail"},
+                new int[]{R.id.itemTitle,R.id.itemDetail}
+                );
+
+    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initListView();
+        MyAdapter myAdapter=new MyAdapter(this,R.layout.list_item,listItems);
+        this.setListAdapter(myAdapter);
+        Thread t = new Thread(this);
+        t.start();
+
+        handler=new Handler(){
+            public void handleMessage(@NonNull Message msg) {
+                if(msg.what==5){
+                    List<HashMap<String,String>> list2=(List<HashMap<String,String>>)msg.obj;
+                    listItemAdapter=new SimpleAdapter(Mylist2Activity.this,list2,
+                            R.layout.list_item,
+                            new String[]{"ItemTitle","ItemDetail"},
+                            new int[]{R.id.itemTitle,R.id.itemDetail}
+                    );
+                    setListAdapter(listItemAdapter);
+
+                }
+                super.handleMessage(msg);
+            }
+        };
+            getListView().setOnItemClickListener(this);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.i(TAG, "onItemClick: parent="+parent);
+        Log.i(TAG, "onItemClick: view="+view);
+        Log.i(TAG, "onItemClick: position="+position);
+        Log.i(TAG, "onItemClick: id="+id);
+        HashMap<String,String>map=(HashMap<String,String>)getListView().getItemAtPosition(position);
+        String titleStr=map.get("ItemTitle");
+        String detailStr=map.get("ItemDetail");
+        Log.i(TAG, "onItemClick: "+titleStr);
+        Log.i(TAG, "onItemClick: "+detailStr);
+        TextView title=view.findViewById(R.id.itemTitle);
+        TextView detail=view.findViewById(R.id.itemDetail);
+        String title2=String.valueOf(title.getText());
+        String detail2=String.valueOf(detail.getText());
+        Log.i(TAG, "onItemClick: title2"+title2);
+        Log.i(TAG, "onItemClick: detail2"+detail2);
+        //打开新的页面传入
+        Intent rateCalc=new Intent(this,RatecalActivity.class);
+        rateCalc.putExtra("title",titleStr);
+        rateCalc.putExtra("rate",Float.parseFloat(detailStr));
+        startActivity(rateCalc);
+
+    }
+}
